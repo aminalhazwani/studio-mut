c0384707bb0dfe4fea1d45cb1de69ad3df8540dc
aminalhazwani <amin.alhazwani@gmail.com>
2014-11-12 22:47:59 +0100

    Minor changes to project template

--- site/templates/project.php
+++ site/templates/project.php
@@ -1,47 +1,35 @@
 <?php snippet('header') ?>
 
-  <main class="main" role="main">
+<main class="main" role="main">
 
-    <h1><?php echo $page->title()->html() ?></h1>
+<h1><?php echo $page->title()->html() ?></h1>
 
-    <ul class="meta cf">
-      <li><b>Year:</b> <time datetime="<?php echo $page->date('c') ?>"><?php echo $page->date('M. Y') ?></time></li>
-      <li><b>Category:</b> <?php echo $page->category() ?></li>
-      <li><b>Client:</b> <?php echo $page->client() ?></li>
-    </ul>
+<ul>
+  <li><b>Year:</b> <time datetime="<?php echo $page->date('c') ?>"><?php echo $page->date('M. Y') ?></time></li>
+  <li><b>Category:</b> <?php echo $page->category() ?></li>
+  <li><b>Client:</b> <?php echo $page->client() ?></li>
+</ul>
 
-    <div class="slider__container">
-      <div class="slider royalSlider rsDefault">
-        <!--
-        <?php echo $page->text()->kirbytext() ?>
-        -->
+<div class="text">
+  <?php echo $page->text()->kirbytext() ?>
 
-        <?php foreach($page->images()->sortBy('sort', 'asc') as $image): ?>
-          <img class ="rsImg" src="<?php echo $image->url() ?>" alt="<?php echo $page->title()->html() ?>">
-        <?php endforeach ?>
-      </div>
-    </div>
+  <?php foreach($page->images()->sortBy('sort', 'asc') as $image): ?>
+  <figure>
+    <img src="<?php echo $image->url() ?>" alt="<?php echo $page->title()->html() ?>">
+    <?php if($image->caption() != ' '): ?>
+        <figcaption><?php echo $image->caption()->kirbytext() ?></figcaption>
+    <?php endif ?>
+  </figure>
+  <?php endforeach ?>
+</div>
 
-    <!--
-    <nav class="nextprev cf" role="navigation">
-      <?php if($prev = $page->prevVisible()): ?>
-      <a class="prev" href="<?php echo $prev->url() ?>">&larr; previous</a>
-      <?php endif ?>
-      <?php if($next = $page->nextVisible()): ?>
-      <a class="next" href="<?php echo $next->url() ?>">next &rarr;</a>
-      <?php endif ?>
-    </nav>
-    -->
+<nav class="nextprev cf" role="navigation">
+  <?php if($prev = $page->prevVisible()): ?>
+  <a class="prev" href="<?php echo $prev->url() ?>">&larr; previous</a>
+  <?php endif ?>
+  <?php if($next = $page->nextVisible()): ?>
+  <a class="next" href="<?php echo $next->url() ?>">next &rarr;</a>
+  <?php endif ?>
+</nav>
 
-  </main>
-
-  <script type="text/javascript">
-    $(function() {
-      $('.slider').royalSlider({
-        autoScaleSlider:false,
-        autoHeight: false,
-        imageScalePadding: 0,
-        keyboardNavEnabled: true
-      });
-    });
-  </script>
+</main>
