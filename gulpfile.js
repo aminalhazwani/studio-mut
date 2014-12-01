f29df1a3f1989608c549da86497326546a65dc7a
aminalhazwani <amin.alhazwani@gmail.com>
2014-12-01 18:35:51 +0100

    Minor changes to gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -20,7 +20,6 @@ gulp.task('styles', function() {
         browsers: ['last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'],
         cascade: false
     }))
-    .pipe(gulp.dest('assets/styles'))
     .pipe(rename({suffix: '.min'}))
     .pipe(minifycss())
     .pipe(gulp.dest('assets/styles'))
