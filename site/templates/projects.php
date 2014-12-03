412d52e89b8ad7e00e0db9cbefb2c94490a2d95a
aminalhazwani <amin.alhazwani@gmail.com>
2014-12-03 20:38:52 +0100

    Cleaned projects template to scratch

--- site/templates/projects.php
+++ site/templates/projects.php
@@ -1,16 +1,22 @@
 <?php snippet('header') ?>
 
-<main class="main" role="main">
-
-	<div class="text">
-	<h1><?php echo $page->title()->html() ?></h1>
-	<?php echo $page->text()->kirbytext() ?>
+<main class="projects" role="main">
+	<div>
+		<h1><?php echo $page->title()->html() ?></h1>
+		<?php echo $page->text()->kirbytext() ?>
 	</div>
 
-	<button id="ChangeLayout">List</button>
-
-	<?php snippet('projects') ?>
-
+	<?php foreach(page('projects')->children()->visible() as $project): ?>
+        <div class="projects__item mix">
+            <a class="projects__item--link"  href="<?php echo $project->url() ?>/">
+                <p class="projects__item--title"><?php echo html($project->title()) ?></p>
+                <!-- 
+                <p class="project__item--client"><?php echo html($project->client()) ?></p>
+                <p class="project__item--date"><?php echo html($project->date()) ?></p> 
+            	-->
+            </a>
+        </div>
+    <?php endforeach ?>
 </main>
 
 <?php echo js('assets/scripts/projects.min.js') ?>
