a901c9a1b87aec9422ac2ced9841a2c3817fed04
aminalhazwani <amin.alhazwani@gmail.com>
2014-12-07 01:52:24 +0100

    Minor changes to the footer

--- src/js/about.js
+++ src/js/about.js
@@ -11,3 +11,24 @@ $('.about__team--designer').flowtype({
     minFont   : 16,
     maxFont   : 28
 });
+
+$('.about__info--clients').flowtype({
+    minimum   : 481,
+    maximum   : 2560,
+    minFont   : 16,
+    maxFont   : 28
+});
+
+$('.about__info--publications').flowtype({
+    minimum   : 481,
+    maximum   : 2560,
+    minFont   : 16,
+    maxFont   : 28
+});
+
+$('.about__info--awards').flowtype({
+    minimum   : 481,
+    maximum   : 2560,
+    minFont   : 16,
+    maxFont   : 28
+});
