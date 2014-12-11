492e3c460a273c2396a8d49679a897ce71e98802
aminalhazwani <amin.alhazwani@gmail.com>
2014-12-11 21:19:26 +0100

    Minor changes to footer and minor style changes

--- site/blueprints/site.php
+++ site/blueprints/site.php
@@ -6,15 +6,33 @@ fields:
   title:
     label: Title
     type:  text
+  keywords:
+    label: Keywords
+    type:  tags
   author:
     label: Author
     type:  text
   description:
     label: Description
     type:  textarea
-  keywords:
-    label: Keywords
-    type:  tags
+  street:
+    label: Street
+    type: text
+    width: 1/2
+  city:
+    label: City
+    type: text
+    width: 1/4
+  country:
+    label: Country
+    type: text
+    width: 1/4
+  contact:
+    label: Contact 
+    type: textarea
+  interships:
+    label: Interships 
+    type: textarea
   copyright:
     label: Copyright
     type:  textarea
--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -1,26 +1,24 @@
 	<footer>
 		<div class="footer__brand">
 			<h2>Studio MUT</h2>
-			<p>Studio MUT is a design studio focusing on print, identity, exhibition, and interactive work with clients in art and architecture. The studio was founded in 2014 by Thomas Kronbichler and Martin Kerschbaumer;</p>
+			<p><?php echo $site->description()->html() ?></p>
 		</div>
 		<div class="footer__address">
 			<h3>Visit us</h3>
-			<p>Oswaldweg 1c<p>
-			<p>39100 Bolzano, Italy</p>
-			<p>+39 0471 194 2440</p>
+			<p><?php echo $site->street()->html() ?><p>
+			<p><?php echo $site->city()->html() ?></p>
+			<p><?php echo $site->country()->html() ?></p>
 		</div>
 		<div class="footer__contact">
 			<h3>Contact</h3>
-			<p>If you are willing to work, collaborate or produce with MUT please contact → 
-			<a href="mailto:hello@studiomut.com">hello@studiomut.com</a></p>
+			<?php echo $site->contact()->kirbytext() ?>
 			<h3>Interships</h3>
-			<p>We are looking for young and motivated designer, send your application →
-			<a href="mailto:work@studiomut.com">work@studiomut.com</a>
+			<?php echo $site->interships()->kirbytext() ?>
 			</p>
 		</div>
 		<div class="footer__credit">
 			<p><?php echo $site->copyright()->html() ?></p>
-			<p>Made with shit by Amin Al Hazwani</p>
+			<p>Made by Amin</p>
 		</div>
 	</footer>
 	<?php echo js('assets/scripts/main.min.js') ?>
