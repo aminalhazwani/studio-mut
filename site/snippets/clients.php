50d858edfddf4e0f1725d967b1fae63b61b68e19
aminalhazwani <amin.alhazwani@gmail.com>
2015-04-16 23:19:37 +0200

    Fuck YEAH! I did it. A php loop for clients list

--- site/snippets/clients.php
+++ site/snippets/clients.php
@@ -1,4 +1,20 @@
 <div class="clients">
-	<?php $clients = $pages->find('/clients') ?>
-	<?php echo $clients->text()->kirbytext() ?>
+	<?php $alphas = range('A', 'Z'); ?>
+	<? foreach($alphas as $alpha): ?>
+	<div class="clients__character">
+		<?php foreach($pages->find('/projects')->children()->visible() as $project): ?>
+		<?php if(substr($project->client(), 0, 1) === $alpha): ?>
+			<h3><?php echo $alpha ?></h3>
+		<?php break; ?>
+		<?php endif ?>
+		<?php endforeach ?>
+		<ul>
+		<?php foreach($pages->find('/projects')->children()->visible() as $project): ?>
+				<?php if(substr($project->client(), 0, 1) === $alpha): ?>
+					<li><?php echo html($project->client()) ?></li>
+				<?php endif?>
+		<?php endforeach ?>
+		</ul>
+	</div>
+	<?php endforeach ?>
 </div>
