941d6552ba3dce4c6a8615587af47ed4077d476c
aminalhazwani <amin.alhazwani@gmail.com>
2015-04-16 23:20:22 +0200

    Minor optimisation

--- site/snippets/clients.php
+++ site/snippets/clients.php
@@ -1,16 +1,16 @@
 <div class="clients">
-	<?php $alphas = range('A', 'Z'); ?>
-	<? foreach($alphas as $alpha): ?>
+	<?php $characters = range('A', 'Z'); ?>
+	<? foreach($characters as $character): ?>
 	<div class="clients__character">
 		<?php foreach($pages->find('/projects')->children()->visible() as $project): ?>
-		<?php if(substr($project->client(), 0, 1) === $alpha): ?>
-			<h3><?php echo $alpha ?></h3>
+		<?php if(substr($project->client(), 0, 1) === $character): ?>
+			<h3><?php echo $character ?></h3>
 		<?php break; ?>
 		<?php endif ?>
 		<?php endforeach ?>
 		<ul>
 		<?php foreach($pages->find('/projects')->children()->visible() as $project): ?>
-				<?php if(substr($project->client(), 0, 1) === $alpha): ?>
+				<?php if(substr($project->client(), 0, 1) === $character): ?>
 					<li><?php echo html($project->client()) ?></li>
 				<?php endif?>
 		<?php endforeach ?>
