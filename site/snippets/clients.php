16ebf16eeb9d9ab4d0feac989c9f0aef3946837f
aminalhazwani <amin.alhazwani@gmail.com>
2015-04-17 17:03:06 +0200

    Removed duplicated from client foreach

--- site/snippets/clients.php
+++ site/snippets/clients.php
@@ -2,18 +2,24 @@
 	<?php $characters = range('A', 'Z'); ?>
 	<? foreach($characters as $character): ?>
 	<div class="clients__character">
-		<?php foreach($pages->find('/projects')->children()->visible() as $project): ?>
+		<?php foreach($pages->find('/projects')->children() as $project): ?>
 		<?php if(substr($project->client(), 0, 1) === $character): ?>
 			<h3><?php echo $character ?></h3>
 		<?php break; ?>
 		<?php endif ?>
 		<?php endforeach ?>
-		<ul>
-		<?php foreach($pages->find('/projects')->children()->visible() as $project): ?>
-				<?php if(substr($project->client(), 0, 1) === $character): ?>
-					<li><?php echo html($project->client()) ?></li>
-				<?php endif?>
+		<?php foreach($pages->find('/projects')->children() as $project): {
+			$clients[] = $project->client();
+		}
+		$clients = array_unique($clients);
+		?>
 		<?php endforeach ?>
+		<ul>
+			<?php foreach($clients as $client): ?>
+				<?php if(substr($client, 0, 1) === $character): ?>
+					<li><?php echo html($client) ?></li>
+				<?php endif?>
+			<?php endforeach ?>
 		</ul>
 	</div>
 	<?php endforeach ?>
