b9f0c37f0a6f67ef2c29746433a8df881a475206
aminalhazwani <amin.alhazwani@gmail.com>
2015-04-22 00:26:00 +0200

    Added videoPoster snippet

---
+++ site/snippets/videoPoster.php
@@ -0,0 +1,20 @@
+<?php foreach($page->files() as $file): {
+	if ($file->type() == 'video') {
+		$videos[] = $file;
+	}
+	if ($file->type() == 'image'){
+		$images[] = $file;
+	}
+	// $results = array_intersect($videos, $images);
+	$results = array_unique($images);
+}
+?><?php endforeach ?>
+<?php foreach($results as $result): ?>
+	<?php if($result->type() == 'image'): ?>
+		<?php echo $result->url() ?>
+	<?php endif ?>
+	<?php break; ?>
+<?php endforeach ?>
+
+
+
