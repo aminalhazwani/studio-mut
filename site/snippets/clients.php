23fa4b9d042f06ed569b1e0028a1c39a2437888c
aminalhazwani <amin.alhazwani@gmail.com>
2015-04-22 11:22:15 +0200

    Updated kirby to v2

--- site/snippets/clients.php
+++ site/snippets/clients.php
@@ -1,26 +1,28 @@
 <div class="clients">
 	<?php $characters = range('A', 'Z'); ?>
-	<? foreach($characters as $character): ?>
-	<div class="clients__character">
-		<?php foreach($pages->find('/projects')->children() as $project): ?>
-		<?php if(substr($project->client(), 0, 1) === $character): ?>
-			<h3><?php echo $character ?></h3>
-		<?php break; ?>
-		<?php endif ?>
-		<?php endforeach ?>
-		<?php foreach($pages->find('/projects')->children() as $project): {
-			$clients[] = $project->client();
-		}
-		$clients = array_unique($clients);
-		?>
-		<?php endforeach ?>
-		<ul>
-			<?php foreach($clients as $client): ?>
-				<?php if(substr($client, 0, 1) === $character): ?>
-					<li><?php echo html($client) ?></li>
-				<?php endif?>
+	<?php foreach($characters as $character): ?>
+		<div class="clients__character">
+			<?php foreach($pages->find('/projects')->children() as $project): ?>
+			<?php if(substr($project->client(), 0, 1) === $character): ?>
+				<h3><?php echo $character ?></h3>
+			<?php break; ?>
+			<?php endif ?>
 			<?php endforeach ?>
-		</ul>
-	</div>
+
+			<?php foreach($pages->find('/projects')->children() as $project): {
+				$clients[] = $project->client();
+			}
+			$clients = array_unique($clients);
+			?>
+			<?php endforeach ?>
+
+			<ul>
+				<?php foreach($clients as $client): ?>
+					<?php if(substr($client, 0, 1) === $character): ?>
+						<li><?php echo html($client) ?></li>
+					<?php endif?>
+				<?php endforeach ?>
+			</ul>
+		</div>
 	<?php endforeach ?>
 </div>
