4facdab723a5add658c7ff7867e4d328bf4956b6
aminalhazwani <amin.alhazwani@gmail.com>
2015-05-13 21:19:01 +0200

    optimize about section

--- site/snippets/about.php
+++ site/snippets/about.php
@@ -1,40 +1,71 @@
 <div class="about">
 	<?php $about = $pages->find('/about') ?>
-	<?php echo $about->text()->kirbytext() ?>
-	<?php if($about->hasImages()): ?>
-	<div class="about__images--top">
+	<div class="about__content about__text">
+		<?php echo $about->text()->kirbytext() ?>
+	</div>
+
+	<div class="about__images about__images--top">
+		<?php if($about->hasImages()): ?>
 		<?php foreach($about->images()->sortBy('sort','asc')->limit(2) as $image): ?>
 		<figure class="about__image about__image--top">
 			<img src="<?php echo $image->url() ?>"> 
 		</figure>
 		<?php endforeach ?>
 	</div>
-	<div class="about__images--bottom">
+
+	<div class="about__images about__images--bottom">
 		<?php foreach($about->images()->sortBy('sort','desc')->limit(2) as $image): ?>
 		<figure class="about__image about__image--bottom">
 			<img src="<?php echo $image->url() ?>"> 
 		</figure>
 		<?php endforeach ?>
+		<?php endif ?>
 	</div>
-	<figcaption><?php echo $about->caption() ?></figcaption>
-	<?php endif ?>
 
-	<?php echo $site->title()->kirbytext() ?>
-	<?php echo $about->address()->kirbytext() ?>
-	<?php echo $about->phone()->kirbytext() ?>
-	<?php echo $about->mail()->kirbytext() ?>
+	<figcaption class="about__images--caption"><?php echo $about->caption() ?></figcaption>
 
-	<?php echo $about->social()->kirbytext() ?>
-	<?php echo $about->awards()->kirbytext() ?>
-	<?php echo $about->talks()->kirbytext() ?>
-	<?php echo $about->newsletter()->kirbytext() ?>
-	<?php snippet('newsletter') ?>
-	<?php echo $about->interships()->kirbytext() ?>
+	<div class="about__content about__contacts">
+		<?php echo $site->title()->kirbytext() ?>
+		<?php echo $about->address()->kirbytext() ?>
+		<?php echo $about->phone()->kirbytext() ?>
+		<?php echo $about->mail()->kirbytext() ?>
+	</div>
 
-	<?php echo $about->infomail()->kirbytext() ?>
-	<?php snippet('weather') ?>
-	<h4>Actual time at Mut</h4>
-	<span id="time"></span>
+	<div class="about__content about__social">
+		<?php echo $about->social()->kirbytext() ?>
+	</div>
 
-	<?php echo $about->warning()->kirbytext() ?>
+	<div class="about__content about__awards">
+		<?php echo $about->awards()->kirbytext() ?>
+	</div>
+
+	<div class="about__content about__talks">
+		<?php echo $about->talks()->kirbytext() ?>
+	</div>
+
+	<div class="about__content about__newsletter">
+		<?php echo $about->newsletter()->kirbytext() ?>
+		<?php snippet('newsletter') ?>
+	</div>
+
+	<div class="about__content about__interships">
+		<?php echo $about->interships()->kirbytext() ?>
+	</div>
+
+	<div class="about__content about__info">
+		<?php echo $about->infomail()->kirbytext() ?>
+	</div>
+
+	<div class="about__content about__weather">
+		<!--<?php snippet('weather') ?>-->
+	</div>
+
+	<div class="about__content about__time">
+		<h4>Actual time at Mut</h4>
+		<span id="time"></span>
+	</div>
+
+	<div class="about__content about__warning">
+		<?php echo $about->warning()->kirbytext() ?>
+	</div>
 </div>
