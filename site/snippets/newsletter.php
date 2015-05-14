69fcdc16a969d16a34943d4baf240634e749358b
aminalhazwani <amin.alhazwani@gmail.com>
2015-05-14 19:39:06 +0200

    [minor][fix] newsletter error messages style

--- site/snippets/newsletter.php
+++ site/snippets/newsletter.php
@@ -2,9 +2,8 @@
 	<form action="//studiomut.us9.list-manage.com/subscribe/post?u=5c8bcd73a44ff6dfaa74aa076&amp;id=191bb1e1e1" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
 		<div id="mc_embed_signup_scroll">
 			<div class="mc-field-group">
-			<label for="mce-EMAIL">Email Address </label>
-			<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
-			<input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe">
+			<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL" placeholder="Your e-mail">
+			<input class="email-button" type="submit" value="I'm in!" name="subscribe" id="mc-embedded-subscribe">
 		</div>
 		<div id="mce-responses" class="clear">
 			<div class="response" id="mce-error-response" style="display:none"></div>
