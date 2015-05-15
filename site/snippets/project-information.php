3c7746c696dbb656f2255d8fb1847301f2690240
aminalhazwani <amin.alhazwani@gmail.com>
2015-05-15 19:24:45 +0200

    style project information element

--- site/snippets/project-information.php
+++ site/snippets/project-information.php
@@ -1,7 +1,10 @@
 <div id="info" class="project-information">
-	<a class="close__side-elements"><span class="close__side-elements--line"></span></a>
-	<p class="project-information__title"><?php echo $page->title() ?> Project Information</p>
-	<div class="project-information__content">
-		<?php echo $page->text()->kirbytext() ?>
+	<div class="project-information__container">
+		<a class="close__side-elements"><span class="close__side-elements--line"></span></a>
+		<span class="project-information__title"><?php echo $page->title() ?></span>
+		<span>Project Information</span>
+		<div class="project-information__content">
+			<?php echo $page->text()->kirbytext() ?>
+		</div>
 	</div>
 </div>
