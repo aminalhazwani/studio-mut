2636ecc29ea214723986f7686f3c09b6e04df522
aminalhazwani <amin.alhazwani@gmail.com>
2015-05-18 15:24:32 +0200

    [add] meta tag for ios apperance reset

--- site/snippets/head.php
+++ site/snippets/head.php
@@ -6,6 +6,7 @@
 <head>
 	<meta charset="utf-8" />
   	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"/>
+  	<meta name="format-detection" content="telephone=no">
 
 	<!-- Page title -->
 	<?php if($page->template() != 'home'): ?>
