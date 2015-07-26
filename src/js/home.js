5e61eec0301a47e23e5e71620dc41c05a0b213f3
aminalhazwani <amin.alhazwani@gmail.com>
2015-07-26 17:15:27 +0200

    [scripts] add home and about js functions

---
+++ src/js/home.js
@@ -0,0 +1,15 @@
+var mn = $(".nav");
+var mns = "nav-scrolled";
+var hdr = $('.header-primary').height();
+
+$(window).scroll(function() {
+  if( $(this).scrollTop() > hdr ) {
+    mn.addClass(mns);
+    $('main').addClass('nav-is-fixed');
+  } else {
+    mn.removeClass(mns);
+    $('main').removeClass('nav-is-fixed');
+  }
+});
+
+$('.header-primary').addClass('test');
