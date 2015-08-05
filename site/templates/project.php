881edaf97344771794c23ee11797935a2305e63e
aminalhazwani <amin.alhazwani@gmail.com>
2015-08-05 09:00:54 +0200

    [minor] progress on project template

--- site/templates/project.php
+++ site/templates/project.php
@@ -6,47 +6,15 @@
     </div>
   </header>
 
-  <div class="showcase__slider royalSlider rsDefault">
-    <?php foreach($page->files()->sortBy('sort', 'asc') as $image): ?>
-      <?php if($image->poster() == ''): ?>
-      <?php if($image->measure() == 'enclosed'): ?>
-        <?php if($image->type() != 'video'): ?>
-          <figure class="showcase__slider--slide">
-            <img class="showcase__image--enclosed" src="<?php echo $image->url() ?>">
-            <?php if($image->overtext() != ''): ?>
-              <figcaption class="showcase__image--text"><?php echo $image->overtext()->kirbytext() ?></figcaption>
-            <?php endif ?>
-          </figure>
-        <?php else: ?>
-          <figure class="showcase__slider--slide">
-            <video class="showcase__image--enclosed showcase__image--video" poster="<?php echo $image->name() ?>.jpg">
-              <source src="<?php echo $image->url() ?>" type="video/mp4">
-            </video>
-            <button class="button" type="button" style="position: absolute; z-index: 1">Play</button>
-          </figure>
-        <?php endif ?>
-      <?php endif ?>
-      <?php if($image->measure() == 'full'): ?>
-        <figure class="showcase__slider--slide">
-          <img class="showcase__image--full" src="../../assets/images/1px.png" style="background-image:url('<?php echo $image->url() ?>');">
-            <?php if($image->overtext() != ''): ?>
-              <figcaption class="showcase__image--text"><?php echo $image->overtext()->kirbytext() ?></figcaption>
-            <?php endif ?>
-        </figure>
-      <?php endif ?>
-      <?php endif ?>
+  <div class="slider royalSlider">
+    <?php foreach($page->files() as $image): ?>
+      <figure class="project_figure">
+        <img class="project_image" src="<?php echo $image->url() ?>">
+      </figure>
     <?php endforeach ?>
   </div>
 
   <?php echo js('assets/js/vendor/vendors.min.js') ?>
-  <?php if($page->template() == 'home'): ?>
-    <?php echo js('assets/js/home.min.js') ?>
-  <?php endif ?>
-  <?php if($page->template() == 'project'): ?>
-    <?php echo js('assets/js/project.min.js') ?>
-  <?php endif ?>
-  <?php if($page->template() == 'about'): ?>
-    <?php echo js('assets/js/about.min.js') ?>
-  <?php endif ?>
+  <?php echo js('assets/js/project.min.js') ?>
   </body>
 </html>
--- src/js/project.js
+++ src/js/project.js
@@ -1,6 +1,5 @@
-// This function load and setup the Royal Slider
 $(function() {
-  var sliderJQ = $('.showcase__slider').royalSlider({
+  var sliderJQ = $('.slider').royalSlider({
     autoScaleSlider: false,
     autoHeight: false,
     keyboardNavEnabled: true,
@@ -15,70 +14,27 @@ $(function() {
     numImagesToPreload: 99
   });
   var sliderInstance = sliderJQ.data('royalSlider');
-  var slideCountEl = $('<div class="showcase__count rsSlideCount"></div>').appendTo( '.showcase' );
+  var slideCountEl = $('<div class="project__count rsSlideCount"></div>').appendTo( '.project' );
   function updCount() {
     slideCountEl.html( (sliderInstance.currSlideId+1) + '&nbsp;&nbsp;/&nbsp;&nbsp;' + sliderInstance.numSlides );
   }
   sliderInstance.ev.on('rsAfterSlideChange', updCount);
-  sliderInstance.ev.on('rsAfterSlideChange', pauseAllVideos );
+  // sliderInstance.ev.on('rsAfterSlideChange', pauseAllVideos );
   updCount();
 
   var si = jQuery('.royalSlider').data('royalSlider');
   si.ev.on('rsBeforeMove', function(e, type, action) {
     if(si.currSlideId+1 === si.numSlides && type === "next") {
-      $('.showcase__close').addClass("swing");
+      $('.project__close').addClass("swing");
       setTimeout(function() {
-        $('.showcase__close').removeClass("swing")
+        $('.project__close').removeClass("swing")
       },600);
     }
     if(si.numSlides+si.currSlideId <= si.numSlides && type === "prev") {
-      $('.showcase__close').addClass("swing");
+      $('.project__close').addClass("swing");
       setTimeout(function() {
-      $('.showcase__close').removeClass("swing")
+      $('.project__close').removeClass("swing")
       },600);
     }
   });
-});
-
-
-// These functions handle videos in royal slider
-window.onload = function() {
-  var currentFigure = $('.showcase__slider--slide');
-    currentFigure.each(function() {
-    var video = $(this).children('.showcase__image--video');
-    var playButton = $(this).children('.button');
-    playButton.on("click", function() {
-      if (video.get(0).paused === true) {
-        video.get(0).play();
-        playButton.addClass('play');
-        } 
-        else {
-        video.get(0).pause();
-        playButton.removeClass('play');
-        }
-    });
-  })
-}
-
-function pauseAllVideos() {
-  $('video').each(function() {
-    $(this).get(0).pause();
-    $('.button').removeClass('play');
-  });
-}
-
-$('.button').click(function(e) {
-  e.stopPropagation();
-});
-
-
-// This function redirect to the previous page when 
-// a user clicks on the close button of the slider
-$('.showcase__close').click(function() {
-  var loc = window.location.href,
-  index = loc.indexOf('#');
-  if (index > 0) {
-    window.location = loc.substring(0, index);
-    }
-  history.go(-1);
 });
