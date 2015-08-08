eacbd781e2c13131019e5eb8260e591a062b002d
aminalhazwani <amin.alhazwani@gmail.com>
2015-08-08 15:52:54 +0200

    [add] image srcset to home and project template

--- site/templates/home.php
+++ site/templates/home.php
@@ -17,11 +17,11 @@
 					<?php if($image->cover() == 'ja'): ?>
 						<a href="<?php echo $project->url() ?>">
 							<img src="<?php echo thumb($image, array('width' => 1200, 'height' => 900, 'quality' => 70), false) ?>" alt="<?php echo html($project->title()) ?>" sizes="100vw" 
-									 srcset="<?php echo thumb($image, array('width' => 600, 'height' => 400, 'quality' => 70), false) ?> 600w,
-													 <?php echo thumb($image, array('width' => 800, 'height' => 600, 'quality' => 70), false) ?> 800w,
-													 <?php echo thumb($image, array('width' => 1200, 'height' => 900, 'quality' => 70), false) ?> 1200w,
-													 <?php echo thumb($image, array('width' => 1600, 'height' => 1200, 'quality' => 70), false) ?> 1600w,
-													 <?php echo thumb($image, array('width' => 2560, 'height' => 1500, 'quality' => 70), false) ?> 2560w">
+								srcset="<?php echo thumb($image, array('width' => 600, 'height' => 400, 'quality' => 70), false) ?> 600w,
+											  <?php echo thumb($image, array('width' => 800, 'height' => 600, 'quality' => 70), false) ?> 800w,
+												<?php echo thumb($image, array('width' => 1200, 'height' => 900, 'quality' => 70), false) ?> 1200w,
+												<?php echo thumb($image, array('width' => 1600, 'height' => 1200, 'quality' => 70), false) ?> 1600w,
+												<?php echo thumb($image, array('width' => 2560, 'height' => 1500, 'quality' => 70), false) ?> 2560w">
 						</a>
 					<?php endif ?>
 				<?php endforeach ?>
--- site/templates/project.php
+++ site/templates/project.php
@@ -13,7 +13,12 @@
 
       <?php if($image->measure() == 'enclosed'): ?>
         <figure class="project_figure">
-          <img class="project_image project_image-enclosed" src="<?php echo $image->url() ?>">
+          <img class="project_image project_image-enclosed" src="<?php echo thumb($image, array('width' => 1200, 'height' => 900, 'quality' => 70), false) ?>" sizes="100vw" 
+                srcset="<?php echo thumb($image, array('width' => 600, 'height' => 400, 'quality' => 70), false) ?> 600w,
+                        <?php echo thumb($image, array('width' => 800, 'height' => 600, 'quality' => 70), false) ?> 800w,
+                        <?php echo thumb($image, array('width' => 1200, 'height' => 900, 'quality' => 70), false) ?> 1200w,
+                        <?php echo thumb($image, array('width' => 1600, 'height' => 1200, 'quality' => 70), false) ?> 1600w,
+                        <?php echo thumb($image, array('width' => 2560, 'height' => 1500, 'quality' => 70), false) ?> 2560w">
           <?php if($image->overtext() != ''): ?>
             <figcaption class="project_caption <?php echo $image->overtextcolor() ?>">
               <?php echo $image->overtext()->kirbytext() ?>
