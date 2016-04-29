d8cb7a38527ac591180f59a9bad7f043990067d9
aminalhazwani <amin.alhazwani@gmail.com>
2016-04-29 17:17:52 +0200

    [fix] aside menu item trigger

--- site/snippets/navigation.php
+++ site/snippets/navigation.php
@@ -21,7 +21,11 @@
     <div class="l-container">
       <?php $menuItems = yaml($site->menuItems()) ?>
       <?php foreach($menuItems as $menuItem): ?>
+        <?php if(strpos($menuItem['hyperlink'], '#archive') == true): ?>
+          <a class="nav_item" href="#archive" <?php if($menuItem['target'] == '1'): ?>target="_blank"<?php endif ?>><?php echo $menuItem['label'] ?></a>
+        <?php else: ?>
           <a class="nav_item" href="<?php echo $menuItem['hyperlink'] ?>" <?php if($menuItem['target'] == '1'): ?>target="_blank"<?php endif ?>><?php echo $menuItem['label'] ?></a>
+        <?php endif ?>
       <?php endforeach ?>
     </div>
   </nav>
