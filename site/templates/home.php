820249104412aa608f5c79a4c8685ef8251bcdb9
aminalhazwani <amin.alhazwani@gmail.com>
2016-05-20 16:02:14 +0200

    [feature] cover image in project template

--- site/templates/home.php
+++ site/templates/home.php
@@ -19,25 +19,23 @@
 						<h5 class="u-push-btm-none u-fs-sm"><?php echo html($project->client()) ?></h5>
 					</div>
 				</div>
-				<?php foreach($project->images() as $image): ?>
-					<?php if($image->cover() == '1'): ?>
-						<a href="<?php echo $project->url() ?>">
-              <?php if($image->extension() == 'gif'): ?>
-                <img class="lazyload" src="<?php echo $image->url() ?>">
-              <?php else: ?>
-                <img class="lazyload" 
-                  src="<?php echo thumb($image, array('width' => 1500), false) ?>" 
-                  srcset="<?php echo thumb($image, array('width' => 600), false) ?> 600w,
-                          <?php echo thumb($image, array('width' => 800), false) ?> 800w,
-                          <?php echo thumb($image, array('width' => 1200), false) ?> 1200w,
-                          <?php echo thumb($image, array('width' => 1600), false) ?> 1600w,
-                          <?php echo thumb($image, array('width' => 2560), false) ?> 2560w"
-                  sizes="100vw"
-                >
-              <?php endif ?>
-						</a>
-					<?php endif ?>
-				<?php endforeach ?>
+        <?php $cover = $project->image($project->cover()); ?>
+        <?php $coverUrl = $project->image($project->cover())->url(); ?>
+				<a href="<?php echo $project->url() ?>">
+          <?php if($cover->extension() == 'gif'): ?>
+            <img class="lazyload" src="<?php echo $coverUrl ?>">
+          <?php else: ?>
+            <img class="lazyload" 
+              src="<?php echo thumb($cover, array('width' => 1200), false) ?>" 
+              srcset="<?php echo thumb($cover, array('width' => 600), false) ?> 600w,
+                      <?php echo thumb($cover, array('width' => 800), false) ?> 800w,
+                      <?php echo thumb($cover, array('width' => 1200), false) ?> 1200w,
+                      <?php echo thumb($cover, array('width' => 1600), false) ?> 1600w,
+                      <?php echo thumb($cover, array('width' => 2560), false) ?> 2560w"
+              sizes="100vw"
+            >
+          <?php endif ?>
+				</a>
 			</article>
     <?php endif ?>
 		<?php endforeach ?>
