3d82cd0127d6281b0196aafb841bdf1ab6b9df25
aminalhazwani <amin.alhazwani@gmail.com>
2016-05-21 16:27:32 +0200

    [edit] project cover archive

--- site/snippets/archive.php
+++ site/snippets/archive.php
@@ -11,21 +11,12 @@
       <?php 
         $title = preg_replace('#[ -]+#', '-', $project->title()->html());
         $title = strtolower($title);
+        $projectCover = $project->image($project->cover());
       ?>
-      <article class="g g-sm-6 g-lg-4">
-        <?php foreach($project->images() as $image): ?>
-          <?php if($image->cover() == '1'): ?>
-            <a href="<?php echo $project->url() ?>">
-              <?php if($image->extension() == 'gif'): ?>
-                <div class="embed embed-3-2">
-                  <img class="lazyload" src="<?php echo $image->url() ?>">
-                </div>
-              <?php else: ?>
-                <div class="aside_projectImage embed embed-3-2" style="background-image: url('<?php echo thumb($image, array('width' => 1000), false) ?>')"></div>
-              <?php endif ?>
-            </a>
-          <?php endif ?>
-        <?php endforeach ?>
+      <article class="g g-sm-6 g-lg-4" id="<?php echo $title ?>">
+        <a href="<?php echo $project->url() ?>">
+          <div class="aside_projectImage embed embed-3-2" style="background-image: url('<?php echo thumb($projectCover, array('width' => 800), false) ?>')"></div>
+        </a>
         <h5 class="aside_projectTitle u-fs-xs u-push-top-half u-push-btm"><a href="<?php echo $project->url() ?>"><?php echo $project->title() ?></a></h2>
       </article>
     <?php endif ?>
