7b10f888e3c6505ded4cf920d35267e9e2973d5c
aminalhazwani <amin.alhazwani@gmail.com>
2016-06-01 19:47:19 +0200

    [edit] news banner

--- site/snippets/news.php
+++ site/snippets/news.php
@@ -1,8 +1,18 @@
 <?php foreach($pages->find('/news')->children()->visible() as $news): ?>
   <section <?php if($news->palette() == 'red'): ?>class="bg-red"<?php else: ?>class="bg-green"<?php endif ?>>
     <div class="news <?php if($news->palette() == 'red'): ?>news-red<?php else: ?>news-green<?php endif ?> u-padd-vert-quarter">
-      <p class="news_text u-push-btm-none"><?php echo $news->title() ?></p>
-      <p class="news_tag u-push-btm-none u-padd-vert-quarter"><?php echo $news->label() ?></p>
+      <div class="news_inner">
+        <span class="news_text" data-text="<?php echo $news->title() ?>"><?php echo $news->title() ?></span>
+        <span class="news_text" data-text="<?php echo $news->title() ?>"><?php echo $news->title() ?></span>
+        <span class="news_text" data-text="<?php echo $news->title() ?>"><?php echo $news->title() ?></span>
+        <span class="news_text" data-text="<?php echo $news->title() ?>"><?php echo $news->title() ?></span>
+        <span class="news_text" data-text="<?php echo $news->title() ?>"><?php echo $news->title() ?></span>
+        <span class="news_text" data-text="<?php echo $news->title() ?>"><?php echo $news->title() ?></span>
+        <span class="news_text" data-text="<?php echo $news->title() ?>"><?php echo $news->title() ?></span>
+      </div>
+      <div class="news_tag">
+        <p class="u-push-btm-none"><?php echo $news->label() ?></p>
+      </div>
     </div>
   </section>
 <?php endforeach ?>
