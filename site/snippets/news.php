c084d9d1b5e0f433440aeffb7d536a183e4494a7
aminalhazwani <amin.alhazwani@gmail.com>
2016-06-06 18:20:07 +0200

    [style] news optimisations

--- site/snippets/news.php
+++ site/snippets/news.php
@@ -1,7 +1,11 @@
 <?php foreach($pages->find('/news')->children()->visible() as $news): ?>
   <section <?php if($news->palette() == 'red'): ?>class="u-bg-red"<?php else: ?>class="u-bg-green"<?php endif ?>>
     <div class="news <?php if($news->palette() == 'red'): ?>news-red<?php else: ?>news-green<?php endif ?>">
-      <div class="news_inner" data-text="<?php echo $news->title() ?>"><?php echo $news->title() ?></div>
+      <div class="news_inner" data-text="<?php echo $news->title() ?>">
+        <p class="u-push-btm-none">
+          <?php echo $news->title() ?>
+        </p>
+      </div>
       <div class="news_tag">
         <p class="u-push-btm-none"><?php echo $news->label() ?></p>
       </div>
