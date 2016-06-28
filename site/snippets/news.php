c8ea0cd3bb6bb57994cc3508654cf502eb726d5c
aminalhazwani <amin.alhazwani@gmail.com>
2016-06-28 16:27:00 +0200

    [edit] remove comment

--- site/snippets/news.php
+++ site/snippets/news.php
@@ -1,7 +1,7 @@
 <?php foreach($pages->find('/news')->children()->visible() as $news): ?>
   <section style="background-color: <?php echo $news->bgcolor() ?>; color: <?php echo $news->txtcolor() ?>">
     <a class="u-blk" href="<?php echo $news->link() ?>" target="_black">
-      <div class="news"> <!-- colors needed here? -->
+      <div class="news">
         <div class="news_inner" data-text="<?php echo $news->title() ?>">
           <p class="u-push-btm-none">
             <?php echo $news->title() ?>
